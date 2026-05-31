61f5ff106199888e6e11ecbf7588cbcf9ced68d3
swaggity-swagger <atharva7223@gmail.com>
2026-05-31 23:32:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,110 @@
+console.log("Welcome to Spotify");
+
+
+let songIndex = 0;
+
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+let masterSongName = document.getElementById('masterSongName');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
+
+let songs = [
+    {songName: "Dumbo-Travis Scott", filePath: "songs/Travis Scott - DUMBO [8G8Fn0YRjvM].mp3", coverPath: "covers/1.png"},
+    {songName: "Faith-The Weeknd", filePath: "songs/The Weeknd - Faith (Audio) [RcS_8-a-sMg].mp3", coverPath: "covers/2.png"},
+    {songName: "Die For You-The Weeknd", filePath: "songs/The Weeknd - Die For You [uPD0QOGTmMI].mp3", coverPath: "covers/3.png"},
+    {songName: "Second Hand-Don Toliver", filePath: "songs/Secondhand (feat. Rema) [iHLsnHMVXck].mp3", coverPath: "covers/4.png"},
+    {songName: "Kiss Of Life-Sade", filePath: "songs/Sade - Kiss Of Life - Official - 1993 [MmOau-PMWJk].mp3", coverPath: "covers/5.png"},
+    {songName: "California Gurls-Katy Perry", filePath: "songs/Katy Perry - California Gurls (Lyrics) Feat. Snoop Dogg [S-fe9T28VLM].mp3", coverPath: "covers/6.png"},
+    {songName: "Cant Tell Me Nothing-Kanye West", filePath: "songs/Kanye West - Can't Tell Me Nothing [E58qLXBfLrs].mp3", coverPath: "covers/7.png"},
+    {songName: "I Want It That Way-Backstreet Boys ", filePath: "songs/Backstreet Boys - I Want It That Way (Official HD Video) [4fndeDfaWCg].mp3", coverPath: "covers/8.png"},
+    {songName: "Bound 2-Kanye West", filePath: "songs/Kanye West - Bound 2 [BBAtAM7vtgc].mp3", coverPath: "covers/9.png"},
+    {songName: "E85 Don Toliver", filePath: "songs/Don Toliver - E85 [Official Visualizer] [7GwLnsVUwHY].mp3", coverPath: "covers/10.png"},
+]
+
+let audioElement = new Audio(songs[0].filePath);
+
+songItems.forEach((element, i)=>{ 
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath; 
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName; 
+})
+ 
+
+
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity = 0;
+    }
+})
+audioElement.addEventListener('timeupdate', ()=>{ 
+
+    progress = parseInt((audioElement.currentTime/audioElement.duration)* 100); 
+    myProgressBar.value = progress;
+})
+
+myProgressBar.addEventListener('change', ()=>{
+    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+})
+
+const makeAllPlays = ()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.remove('fa-pause-circle');
+        element.classList.add('fa-play-circle');
+    })
+}
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click', (e)=>{ 
+        makeAllPlays();
+        songIndex = parseInt(e.target.id);
+        e.target.classList.remove('fa-play-circle');
+        e.target.classList.add('fa-pause-circle');
+       audioElement.src = songs[songIndex].filePath;
+        masterSongName.innerText = songs[songIndex].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        gif.style.opacity = 1;
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+    })
+})
+
+document.getElementById('next').addEventListener('click', ()=>{
+    if(songIndex>=9){
+        songIndex = 0
+    }
+    else{
+        songIndex += 1;
+    }
+   audioElement.src = songs[songIndex].filePath;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+
+})
+
+document.getElementById('previous').addEventListener('click', ()=>{
+    if(songIndex<=0){
+        songIndex = 0
+    }
+    else{
+        songIndex -= 1;
+    }
+    audioElement.src = songs[songIndex].filePath;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+})
