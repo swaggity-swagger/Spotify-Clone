ab2a982a252d22980382c50e24cb0ced8aa1069e
swaggity-swagger <atharva7223@gmail.com>
2026-06-01 01:30:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,16 +10,16 @@ let masterSongName = document.getElementById('masterSongName');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let songs = [
-    {songName: "Dumbo-Travis Scott", filePath: "songs/Travis Scott - DUMBO [8G8Fn0YRjvM].mp3", coverPath: "covers/1.png"},
-    {songName: "Faith-The Weeknd", filePath: "songs/The Weeknd - Faith (Audio) [RcS_8-a-sMg].mp3", coverPath: "covers/2.png"},
-    {songName: "Die For You-The Weeknd", filePath: "songs/The Weeknd - Die For You [uPD0QOGTmMI].mp3", coverPath: "covers/3.png"},
-    {songName: "Second Hand-Don Toliver", filePath: "songs/Secondhand (feat. Rema) [iHLsnHMVXck].mp3", coverPath: "covers/4.png"},
-    {songName: "Kiss Of Life-Sade", filePath: "songs/Sade - Kiss Of Life - Official - 1993 [MmOau-PMWJk].mp3", coverPath: "covers/5.png"},
-    {songName: "California Gurls-Katy Perry", filePath: "songs/Katy Perry - California Gurls (Lyrics) Feat. Snoop Dogg [S-fe9T28VLM].mp3", coverPath: "covers/6.png"},
-    {songName: "Cant Tell Me Nothing-Kanye West", filePath: "songs/Kanye West - Can't Tell Me Nothing [E58qLXBfLrs].mp3", coverPath: "covers/7.png"},
-    {songName: "I Want It That Way-Backstreet Boys ", filePath: "songs/Backstreet Boys - I Want It That Way (Official HD Video) [4fndeDfaWCg].mp3", coverPath: "covers/8.png"},
-    {songName: "Bound 2-Kanye West", filePath: "songs/Kanye West - Bound 2 [BBAtAM7vtgc].mp3", coverPath: "covers/9.png"},
-    {songName: "E85 Don Toliver", filePath: "songs/Don Toliver - E85 [Official Visualizer] [7GwLnsVUwHY].mp3", coverPath: "covers/10.png"},
+    {songName: "Dumbo-Travis Scott", filePath: "songs/Travis Scott - DUMBO [8G8Fn0YRjvM].mp3", coverPath: "1.png"},
+    {songName: "Faith-The Weeknd", filePath: "songs/The Weeknd - Faith (Audio) [RcS_8-a-sMg].mp3", coverPath: "2.png"},
+    {songName: "Die For You-The Weeknd", filePath: "songs/The Weeknd - Die For You [uPD0QOGTmMI].mp3", coverPath: "3.png"},
+    {songName: "Second Hand-Don Toliver", filePath: "songs/Secondhand (feat. Rema) [iHLsnHMVXck].mp3", coverPath: "4.png"},
+    {songName: "Kiss Of Life-Sade", filePath: "songs/Sade - Kiss Of Life - Official - 1993 [MmOau-PMWJk].mp3", coverPath: "5.png"},
+    {songName: "California Gurls-Katy Perry", filePath: "songs/Katy Perry - California Gurls (Lyrics) Feat. Snoop Dogg [S-fe9T28VLM].mp3", coverPath: "6.png"},
+    {songName: "Cant Tell Me Nothing-Kanye West", filePath: "songs/Kanye West - Can't Tell Me Nothing [E58qLXBfLrs].mp3", coverPath: "7.png"},
+    {songName: "I Want It That Way-Backstreet Boys ", filePath: "songs/Backstreet Boys - I Want It That Way (Official HD Video) [4fndeDfaWCg].mp3", coverPath: "8.png"},
+    {songName: "Bound 2-Kanye West", filePath: "songs/Kanye West - Bound 2 [BBAtAM7vtgc].mp3", coverPath: "9.png"},
+    {songName: "E85 Don Toliver", filePath: "songs/Don Toliver - E85 [Official Visualizer] [7GwLnsVUwHY].mp3", coverPath: "10.png"},
 ]
 
 let audioElement = new Audio(songs[0].filePath);
